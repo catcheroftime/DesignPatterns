5f2823d8c8a1b18acc41c13d3f13b9c6556649d1
zw <1500098266@qq.com>
2021-05-07 18:25:27 +0800

    Update main.cpp

--- Iterator/main.cpp
+++ Iterator/main.cpp
@@ -4,16 +4,15 @@ using namespace std;
 
 void test()
 {
-    vector<int> v;  //v是存放int类型变量的可变长数组，开始时没有元素
+    vector<int> v;
     for (int n = 0; n<5; ++n)
-        v.push_back(n);  //push_back成员函数在vector容器尾部添加一个元素
-    vector<int>::iterator i;  //定义正向迭代器
-    for (i = v.begin(); i != v.end(); ++i) {  //用迭代器遍历容器
-        cout << *i << " ";  //*i 就是迭代器i指向的元素
-        *i *= 2;  //每个元素变为原来的2倍
+        v.push_back(n);
+    vector<int>::iterator i;
+    for (i = v.begin(); i != v.end(); ++i) {
+        cout << *i << " ";
+        *i *= 2;
     }
     cout << endl;
-    //用反向迭代器遍历容器
     for (vector<int>::reverse_iterator j = v.rbegin(); j != v.rend(); ++j)
         cout << *j << " ";
 }
