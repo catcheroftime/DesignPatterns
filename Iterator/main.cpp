7c5e8ac345dac9b04111e53f4561496ea3868656
zw <1500098266@qq.com>
2021-05-11 14:36:41 +0800

    迭代器模式

--- Iterator/main.cpp
+++ Iterator/main.cpp
@@ -1,51 +1,44 @@
+#include <memory>
 #include <iostream>
 #include <list>
 using namespace std;
 
-void test()
+struct A {
+    A* operator &() {return nullptr;}
+};
+
+void testAddressof()
+{
+    A a;
+    std::cout << &a << endl;
+    std::cout << std::__addressof(a) << endl;
+}
+
+void testIterator()
 {
     list<int> v;
     for (int n = 0; n<8; ++n)
         v.push_back(n);
 
     list<int>::iterator i;
-
     for (i = v.begin() ;i != v.end(); ++i) {
         cout << *i << " ";
         *i *= 2;
     }
+
     cout << endl;
     for (i = v.begin() ;i != v.end(); ++i) {
         cout << *i << " ";
     }
 
     cout << endl;
-    cout << *i << endl;
+    cout << *v.end() << endl;
 }
 
-class A
-{
-public:
-    A() {}
-    virtual ~A() {}
-
-private:
-    int c;
-    char a;
-
-};
-
-
-struct  B
-{
-    int a;
-    float b;
-    double c;
-    char d;
-};
 
 int main()
 {
-    test();
+//    testAddressof();
+    testIterator();
     return 0;
 }
